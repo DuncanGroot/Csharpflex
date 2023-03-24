24074190bb55f22e0147d072c07b59c347c59efd
Duncan Groot <34777@ma-web.nl>
2023-03-24 12:42:08 +0100

    added date and time

---
+++ Time/Time/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current Date and Time is:");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+        }
+    }
+}
