55471dc30aa0dc0bf4d71fa3261d3805a7c683c3
Duncan Groot <34777@ma-web.nl>
2023-04-14 12:20:13 +0200

    corrected incorrect commit

---
+++ HelloWorldG/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("H");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("He");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hel");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hell");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello,");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello, W");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello, Wo");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello, Wor");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello, Worl");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello, World");
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Hello, World!");
+            // this is horribly inefficient but I wanna get over with this
+        }
+    }
+}
